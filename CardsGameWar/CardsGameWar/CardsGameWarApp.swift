d8194b7d85d7ad21c8277c7bc51509ecf08841d3
Mai Ra <mra1@mail.sfsu.edu>
2021-12-06 17:32:44 -0800

    real project

---
+++ CardsGameWar/CardsGameWar/CardsGameWarApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardsGameWarApp.swift
+//  CardsGameWar
+//
+//  Created by Mai Ra on 12/6/21.
+//
+
+import SwiftUI
+
+@main
+struct CardsGameWarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
